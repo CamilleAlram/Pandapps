52882a53099319d16236b694c3789264d946732b
camillealram <camillealram@hotmail.fr>
2016-12-19 13:39:18 +0100

    Fixe les settings et config

--- panda/panda/settings.py
+++ panda/panda/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pandapps'
+    'debug_toolbar',
+    'pandapps',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'panda.urls'
