71f8faad3a89742b2b605a7fc902df7f6f63afda
camillealram <camillealram@hotmail.fr>
2017-01-05 09:57:42 +0100

    Ajout du lien vers le nouveau template de contact

--- panda/panda/urls.py
+++ panda/panda/urls.py
@@ -5,4 +5,5 @@ from pandapps import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.HomePage.as_view(), name='home'),
+    url(r'^contact/$', views.ContactPage.as_view(), name='contact'),
 ]
