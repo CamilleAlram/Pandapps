efaefa89b58a717ed825252b064b10ff68e1f2fd
camillealram <camillealram@hotmail.fr>
2017-02-07 16:25:40 +0100

    Restructuration

--- panda/panda/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from pandapps import views
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.HomePage.as_view(), name='home'),
-    url(r'^contact/$', views.ContactPage.as_view(), name='contact'),
-]
